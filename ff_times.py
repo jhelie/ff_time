f8fe9e61e6e6f228e3e1a26988264c1cc367635d
jhelie <jean.helie@gmail.com>
2015-07-21 09:26:44 +0100

    v0.0.3

--- ff_times.py
+++ ff_times.py
@@ -681,7 +681,7 @@ def check_ff(f_nb, t, box_dim):
 def write_sele_update():
 	
 	print " -updating selection file '" + str(args.selection_file_ff) + "'..."	
-	filename_txt = os.getcwd() + '/' + str(args.output_folder) + '/' + str(args.selection_file_ff[:-4].split('/')[-1]) + '_update.sele'
+	filename_txt = os.getcwd() + '/' + str(args.output_folder) + '/' + str(args.selection_file_ff[:-5].split('/')[-1]) + '_update.sele'
 	output_txt = open(filename_txt, 'w')
 	#upper to lower
 	for l_index in lipids_ff_u2l_index:
