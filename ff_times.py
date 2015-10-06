31b0bdb41fb41b82a2ae075e332fda6da44c0988
jhelie <jean.helie@gmail.com>
2015-10-06 11:18:40 +0100

    v0.0.3

--- ff_times.py
+++ ff_times.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser(prog='ff_times', usage='', add_help=False, form
 '''
 **********************************************
 v''' + version_nb + '''
-author: Jean Helie (jean.helie@bioch.ox.ac.uk)
+author: Jean Helie (jean.helie@gmail.com)
 git: https://github.com/jhelie/ff_times
 **********************************************
 	
